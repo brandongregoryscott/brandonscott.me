10cc033a91c27f5c64a2ca9ba3d82d842a1d7efc
Brandon Scott <brandongregoryscott@gmail.com>
2021-07-25 14:46:18 -0400

    Initial commit from gatsby lumen starter

---
+++ config.js
@@ -0,0 +1,46 @@
+"use strict";
+
+module.exports = {
+    url: "https://www.brandonscott.me",
+    pathPrefix: "/",
+    title: "Brandon Scott",
+    subtitle: "Portfolio",
+    copyright: "© All rights reserved.",
+    disqusShortname: "",
+    postsPerPage: 4,
+    googleAnalyticsId: "G-VHHBZ8V9P5",
+    useKatex: false,
+    menu: [
+        {
+            label: "Blog",
+            path: "/",
+        },
+        {
+            label: "About",
+            path: "/pages/about",
+        },
+    ],
+    author: {
+        name: "Brandon Scott",
+        photo: "/photo.jpg",
+        bio: "Software engineer from Lancaster, PA.",
+        contacts: {
+            email: "brandon@brandonscott.me",
+            facebook: "",
+            telegram: "",
+            twitter: "",
+            github: "brandongregoryscott",
+            rss: "",
+            vkontakte: "",
+            linkedin: "brandongregoryscott",
+            instagram: "",
+            line: "",
+            gitlab: "",
+            weibo: "",
+            codepen: "",
+            youtube: "",
+            soundcloud: "",
+            medium: "",
+        },
+    },
+};
