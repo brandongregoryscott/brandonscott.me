720ae527d24e3750daad826d8b1b38f595c68999
Brandon Scott <brandongregoryscott@gmail.com>
2021-08-14 16:39:26 -0400

    Plumb through 'showTitle' prop/option for pages, fix non-existent option 'optional' w/ actual 'required'

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -2,12 +2,13 @@ import React, { useRef, useEffect, PropsWithChildren } from "react";
 import styles from "./Page.module.scss";
 
 type Props = {
+    showTitle?: boolean;
     title?: string;
 };
 
 const Page = (props: PropsWithChildren<Props>) => {
-    const { title, children } = props;
-    const pageRef = useRef();
+    const { title, showTitle, children } = props;
+    const pageRef = useRef<HTMLDivElement>();
 
     useEffect(() => {
         pageRef.current.scrollIntoView();
@@ -16,7 +17,9 @@ const Page = (props: PropsWithChildren<Props>) => {
     return (
         <div ref={pageRef} className={styles["page"]}>
             <div className={styles["page__inner"]}>
-                {title && <h1 className={styles["page__title"]}>{title}</h1>}
+                {title != null && showTitle === true && (
+                    <h1 className={styles["page__title"]}>{title}</h1>
+                )}
                 <div className={styles["page__body"]}>{children}</div>
             </div>
         </div>
